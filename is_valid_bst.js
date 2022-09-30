005a4ea69544f2c69186f151b69c600096e4bd3c
juconghe <hejucong1995@gmail.com>
2022-09-29 20:52:32 -0400

    solution

---
+++ is_valid_bst.js
@@ -0,0 +1,28 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+    let prev = null;
+    const inorder = function (root) {
+        if (root === null) {
+            return true;
+        }
+        if (!inorder(root.left)) {
+            return false;
+        }
+        if (prev != null && root.val <= prev) {
+            return false;
+        }
+        prev = root.val;
+        return inorder(root.right);
+    }
+    return inorder(root);
+};
+
+isValidBST(new TreeNode(0, null, new TreeNode(-1)));
